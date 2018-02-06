aa6ca7d82ada0885deb2bafe2c5882dbe2050774
Xavier Petit <le@vieux.pro>
2018-02-06 13:07:29 +0100

    13:00 commit

--- app.js
+++ app.js
@@ -4,34 +4,68 @@ const path = require('path')
 const { promisify } = require('util')
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
+const readdir = promisify(fs.readdir)
+const frontend = restify.createServer()
+const backend = restify.createServer()
 
-const server = restify.createServer()
-server.use(restify.plugins.bodyParser())
-// server.use(restify.CORS())
+frontend.get('/\/.*/', restify.plugins.serveStatic({
+    directory: __dirname + "/public/",
+    default: './index.html'
+   })
+)
+
+backend.use(restify.plugins.bodyParser())
+
+backend.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type")
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Credentials', true)
+    next()
+  })
 
 const rootDir = process.env.ROOT_DIR || `${__dirname}/articles`
 
+try {
+  fs.mkdirSync(rootDir)
+} catch (err) {
+  if (err.code !== 'EEXIST')
+    throw err
+}
+
 const asyncWrapper = fn => (req, res, next) => fn(req)
-	.then(result => {
-		res.send(result)
-		next()
-	}, err => {
-		console.error(err)
-		next(err)
-	})
+  .then(result => {
+    res.send(result)
+    next()
+  }, err => {
+    console.error(err)
+    next(err)
+  })
 
 const respond = (req, res, next) => {
   res.send('hello ' + req.params.name)
   next()
 }
 
-server.get('/article/:id', asyncWrapper(async ({ params }) =>
-	readFile(path.join(rootDir, params.id), 'utf8')))
+const getId = () => readdir(rootDir)
+  .then(idList => idList
+    .map(Number)
+    .sort((a, b) => b - a)[0] + 1)
 
-server.post('/article/:id', asyncWrapper(async ({ params, body }) =>
-	writeFile(path.join(rootDir, params.id), body.content, 'utf8')))
+backend.get('/article/', asyncWrapper(({ params }) =>
+  readdir(rootDir)))
 
+backend.get('/article/:id', asyncWrapper(({ params }) =>
+  readFile(path.join(rootDir, params.id), 'utf8')))
 
-server.listen(8080, () => {
-  console.log(`${server.name} listening at ${server.url}`)
+backend.post('/article/:id', asyncWrapper(({ params, body }) =>
+  writeFile(path.join(rootDir, params.id), body.content, 'utf8')))
+
+backend.post('/article/', asyncWrapper(async ({ body }) =>
+  writeFile(path.join(rootDir, await getId()), body.content, 'utf8')))
+
+backend.listen(8080, () => {
+  console.log(`${backend.name} listening at ${backend.url}`)
 })
+
+frontend.listen(80)
