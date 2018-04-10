4926d070fe5997888898fcc82a21ee04965ae2c4
Xavier Petit <le@vieux.pro>
2018-04-10 18:38:29 +0200

    cleanup

--- app.js
+++ app.js
@@ -5,34 +5,8 @@ const { promisify } = require('util')
 const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
 const readdir = promisify(fs.readdir)
-const frontend = restify.createServer()
-const backend = restify.createServer()
-const corsMiddleware = require('restify-cors-middleware')
-
-frontend.get('/\/.*/', restify.plugins.serveStatic({
-    directory: __dirname + "/public/",
-    default: './index.html'
-   })
-)
-
-const cors = corsMiddleware({
-  preflightMaxAge: 5,
-  origins: ['*']
-})
-
-backend.pre(cors.preflight);
-backend.use(cors.actual);
-backend.use(restify.plugins.bodyParser())
-
-// backend.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*")
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type")
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-//     res.header('Access-Control-Allow-Credentials', true)
-//     next()
-//   })
-
-const rootDir = process.env.ROOT_DIR || `${__dirname}/articles`
+const server = restify.createServer()
+const rootDir = `${__dirname}/articles`
 
 try {
   fs.mkdirSync(rootDir)
@@ -50,29 +24,28 @@ const asyncWrapper = fn => (req, res, next) => fn(req)
     next(err)
   })
 
-const respond = (req, res, next) => {
-  res.send('hello ' + req.params.name)
-  next()
-}
-
 const getId = () => readdir(rootDir)
   .then(idList => idList.length
     ? idList.map(Number).sort((a, b) => b - a)[0] + 1
     : 1)
 
-backend.get('/article/', asyncWrapper(({ params }) =>
-  readdir(rootDir)))
+server.use(restify.plugins.bodyParser())
 
-backend.get('/article/:id', asyncWrapper(({ params }) =>
-  readFile(path.join(rootDir, params.id), 'utf8')))
+server.get('/', restify.plugins.serveStatic({
+  directory: __dirname + '/public/',
+  default: './index.html'
+}))
 
-backend.post('/article/', asyncWrapper(async ({ body }) => {
-  await writeFile(path.join(rootDir, body.id || String(await getId())), body.content, 'utf8')
+server.get('/article/', asyncWrapper(() => readdir(rootDir)))
+
+server.get('/article/:id', asyncWrapper(req =>
+  readFile(path.join(rootDir, req.params.id), 'utf8')))
+
+server.post('/article/', asyncWrapper(async req => {
+  await writeFile(path.join(rootDir, String(await getId())), req.body.content, 'utf8')
   return 'OK'
 }))
 
-backend.listen(8080, () => {
-  console.log(`${backend.name} listening at ${backend.url}`)
+server.listen(80, () => {
+  console.log(`${server.name} listening at ${server.url}`)
 })
-
-frontend.listen(80)
